75440565507bf5a4e2e2ef824edc80f795b002af
scvit <88419463+scvit@users.noreply.github.com>
2024-12-18 14:44:22 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "vpc_module" {
+  source  = "mwtfe.ddimtech.click/org/vpc_module/aws"
+  version = "1.0.0"
+  # insert required variables here
+  vpc_cidr = "10.55.0.0/16"
+  vpc_name = "testvpc-mw"
+  
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  }
