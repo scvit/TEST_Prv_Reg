f224e056e230313eed8761db5d12782610fc9092
scvit <88419463+scvit@users.noreply.github.com>
2024-12-18 16:39:16 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "vpc_module" {
   source  = "mwtfe.ddimtech.click/org/vpc_module/aws"
-  version = "1.0.0"
+  version = "1.0.1"
   # insert required variables here
-  vpc_cidr = "10.52.0.0/16"
+  vpc_cidr = "10.54.0.0/16"
   vpc_name = "testvpc-mw-change"
   region = "ap-northeast-2"
   access_key = var.access_key
